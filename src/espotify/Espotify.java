ae14bbfd56c3bf1135b348d4c96f1288ca36882d
leandro <leandro.rodriguez@fing.edu.uy>
2016-08-12 19:35:44 -0300

    solo por si tengo cambios

--- src/espotify/Espotify.java
+++ src/espotify/Espotify.java
@@ -15,7 +15,7 @@ public class Espotify {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here ESTE MAIN VUELA DESPUES Y LA APP ARRANCA DESDE EL FRAME.. LO VEMOS
     }
     
 }
