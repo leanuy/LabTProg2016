642068cbc9975bf76581cdf2e4b0a05c9841721b
JavierM42 <JavierM42@javierm42s-mbp>
2016-08-21 18:03:37 -0300

    conflictos

--- src/Presentacion/AltaAlbum.java
+++ src/Presentacion/AltaAlbum.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Presentacion;
 
 import javax.swing.DefaultListModel;
@@ -10,10 +5,6 @@ import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.ListModel;
 
-/**
- *
- * @author leandro
- */
 public class AltaAlbum extends javax.swing.JInternalFrame {
     DefaultListModel modeloTemas; 
     /**
