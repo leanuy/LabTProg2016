6737674bcdcd18a2e2f213245fa4a5c2a336cfa8
JavierM42 <JavierM42@javierm42s-mbp>
2016-08-25 14:19:39 -0300

    Alta de Lista con árboles.

--- src/Presentacion/AltaGenero.java
+++ src/Presentacion/AltaGenero.java
@@ -131,7 +131,7 @@ public class AltaGenero extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 24, Short.MAX_VALUE))
         );
 
         pack();
