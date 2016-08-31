acea6ea81a8681cfccfe1d46c4c76bdc57aa9f66
JavierM42 <JavierM42@javierm42s-mbp>
2016-08-31 15:43:03 -0300

    Datos de prueba (sin terminar) + GUI.

--- src/espotify/CtrlDatosIniciales.java
+++ src/espotify/CtrlDatosIniciales.java
@@ -1,5 +1,6 @@
 package espotify;
 
+import espotify.Datatypes.DataArtista;
 import espotify.Datatypes.DataCliente;
 import espotify.Datatypes.DataDefecto;
 import espotify.Datatypes.DataGenero;
@@ -37,8 +38,80 @@ public abstract class CtrlDatosIniciales {
         dataCliente = new DataCliente("lachiqui", "Mirtha", "Legrand", "lachiqui@hotmail.com.ar", cal, null);
         ctrlAltaPerfil.AltaCliente(dataCliente);
         cal.set(1937, 5, 8);
+        dataCliente = new DataCliente("cbochinche", "Cacho", "Bochinche", "cbochinche@vera.com.uy", cal, null);
+        ctrlAltaPerfil.AltaCliente(dataCliente);
+        cal.set(1971, 12, 31);
         dataCliente = new DataCliente("Eleven11", "Eleven", " ", "Eleven11@gmail.com", cal, null);
         ctrlAltaPerfil.AltaCliente(dataCliente);
+        
+        DataArtista dataArtista;      
+        String bio;
+        
+        cal.set(1, 1, 1977);
+        bio = "Village People es una innovadora formación musical de estilo disco de finales de los años 70. Fue famosa tanto por sus peculiares disfraces, como por sus canciones pegadizas, con letras sugerentes y llenas de dobles sentidos.";
+        dataArtista = new DataArtista(bio, "www.officialvillagepeople.com", "vpeople", "Village", "People", "vpeople@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(14, 6, 1980);
+        bio = "";
+        dataArtista = new DataArtista(bio, "www.depechemode.com", "dmode", "Depeche", "Mode", "dmode@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(22, 6, 1953);
+        bio = "Cynthia Ann Stephanie Lauper, conocida simplemente como Cyndi Lauper, es una cantautora, actriz y empresaria estadounidense. Después de participar en el grupo musical, Blue Angel, en 1983 firmó con Portrait Records (filial de Epic Records) y lanzó su exitoso álbum debut She's So Unusual a finales de ese mismo año. Siguió lanzando una serie de álbumes en los que encontró una inmensa popularidad, superando los límites de contenido de las letras de sus canciones.";
+        dataArtista = new DataArtista(bio, "cyndilauper.com", "clauper", "Cyndi", "Lauper", "clauper@hotmail.com", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(23, 9, 1949);
+        bio = "";
+        dataArtista = new DataArtista(bio, "brucespringsteen.net", "bruceTheBoss", "Bruce", "Springsteen", "bruceTheBoss@gmail.com", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(1, 1, 1998);
+        bio = "La Triple Nelson es un grupo de rock uruguayo formado en enero de 1998 e integrado inicialmente por Christian Cary (guitarra y voz), Fernando Paco"
+        +" Pintos (bajo y coros) y Rubén Otonello (actualmente su nuevo baterista es Rafael Ugo).";
+        dataArtista = new DataArtista(bio, "", "tripleNelson", "La Triple", "Nelson", "tripleNelson@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(14, 2, 1987);
+        bio = "";
+        dataArtista = new DataArtista(bio, "", "la_ley", "La", "Ley", "la_ley@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(7, 6, 1940);
+        bio = "Sir Thomas John, conocido por su nombre artístico de Tom Jones, es un cantante británico. Ha vendido más de 100 millones de discos en todo el mundo.";
+        dataArtista = new DataArtista(bio, "", "tigerOfWales", "Tom", "Jones", "tigerOfWales@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(25, 4, 1840);
+        bio = "Piotr Ilich Chaikovski fue un compositor ruso del período del Romanticismo";
+        dataArtista = new DataArtista(bio, "", "chaiko", "Piotr", "Tchaikovsky", "chaiko@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(31, 10, 1980);
+        bio = "";
+        dataArtista = new DataArtista(bio, "", "nicoleneu", "Nicole", "Neumann", "nicoleneu@hotmail.com", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(13, 8, 1981);
+        bio = "";
+        dataArtista = new DataArtista(bio, "www.pimpinela.net", "lospimpi", "Pimpinela", " ", "lospimpi@gmail.com", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(5, 3, 1940);
+        bio = "José Gómez Romero, conocido artísticamente como Dyango es un cantante español de música romántica.";
+        dataArtista = new DataArtista(bio, "", "dyangounchained", "Dyango", " ", "dyangounchained@gmail.com", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+
+        cal.set(17, 7, 1952);
+        bio = "Su carrera comienza en 1976 cuando forma la banda Los Playeros junto a su hermano Víctor. Al poco tiempo se mudan a San Luis donde comienzan a hacerse conocidos en la escena musical. Su éxito a nivel nacional llega a comienzos de los años 1990 cuando desembarca en Buenos Aires y graba el éxito Violeta"
+        + ", originalmente compuesta e nterpretada en 1985 por el músico brasileño Luiz Caldas bajo el título «Fricote».";
+        dataArtista = new DataArtista(bio, "", "alcides", "Alcides", " ", "alcides@tuta.io", cal, null);
+        ctrlAltaPerfil.AltaArtista(dataArtista);
+        
+        
+        
+        
         
         //ALTA DE SEGUIDORES
         IAltaSeguir ctrlSeguir = Fabrica.getIAltaSeguir();
@@ -106,6 +179,34 @@ public abstract class CtrlDatosIniciales {
         ctrlSeguir.AltaSeguir("Eleven11", "ppArgento");
         
         
+        //ALTA DE GENEROS
+        IAltaGenero ctrlAltaGenero = Fabrica.getIAltaGenero();
+        DataGenero dataGenero = new DataGenero("Rock", "");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Rock Clásico", "Rock");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Rock Latino", "Rock");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Rock & Roll", "Rock");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Clásica", "");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Disco", "");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Pop", "");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Electropop", "Pop");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Dance-pop", "Pop");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Pop Clásico", "Pop");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Balada", "");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        dataGenero = new DataGenero("Cumbia", "");
+        ctrlAltaGenero.AltaGenero(dataGenero);
+        
+        
         //ALTA DE LISTAS
         //DEFECTO
         IAltaLista ctrlAltaListas = Fabrica.getIAltaLista();
@@ -148,35 +249,7 @@ public abstract class CtrlDatosIniciales {
         
         
         
-        //ALTA DE GENEROS
-        IAltaGenero ctrlAltaGenero = Fabrica.getIAltaGenero();
-        DataGenero dataGenero = new DataGenero("Rock", "");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Rock Clásico", "Rock");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Rock Latino", "Rock");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Rock & Roll", "Rock");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Clásica", "");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Disco", "");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Pop", "");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Electropop", "Pop");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Dance-pop", "Pop");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Pop Clásico", "Pop");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Balada", "");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        dataGenero = new DataGenero("Cumbia", "");
-        ctrlAltaGenero.AltaGenero(dataGenero);
-        
-        
-        
+ 
     }
     
     
