e2160ab647ca7a694b0620f86d072ac60b921851
JavierM42 <JavierM42@javierm42s-mbp>
2016-09-19 19:10:09 -0300

    Validación de nick y correo con Ajax.

--- servidor_central/src/espotify/CtrlUsuarios.java
+++ servidor_central/src/espotify/CtrlUsuarios.java
@@ -41,6 +41,7 @@ import espotify.interfaces.IDesFavoritear;
 import espotify.interfaces.IFavoritear;
 import espotify.interfaces.IIniciarSesion;
 import espotify.interfaces.web.IListarArtistas;
+import espotify.interfaces.web.IValidar;
 import espotify.interfaces.web.IVerPerfil;
 import espotify.interfaces.web.IWebSeguir;
 
@@ -52,7 +53,7 @@ import java.util.Map.Entry;
 
 public class CtrlUsuarios implements IDesFavoritear, IConsultaCliente, IConsultaArtista,
         IAltaSeguir, IDejarDeSeguir, IAltaPerfil, IFavoritear, IActualizarSuscripcion,
-        IVerPerfil, IIniciarSesion, IWebSeguir, IListarArtistas{
+        IVerPerfil, IIniciarSesion, IWebSeguir, IListarArtistas, IValidar{
 //Constructor
     public CtrlUsuarios() {
     }
@@ -147,7 +148,8 @@ public class CtrlUsuarios implements IDesFavoritear, IConsultaCliente, IConsulta
     }
     
     @Override
-    public List<String> listarListasPrivadasDeCliente(String nick) throws ClienteInexistenteException {
+    public List<String> listarListasPrivadasDeCliente(String nick)
+            throws ClienteInexistenteException {
         return buscarCliente(nick).listarListasPrivadas();
     }
     
@@ -218,7 +220,8 @@ public class CtrlUsuarios implements IDesFavoritear, IConsultaCliente, IConsulta
         return buscarArtista(nick).getDataArtistaExt();
     }
     
-    private boolean existeUsuarioCorreo(String correo) {
+    @Override
+    public boolean existeUsuarioCorreo(String correo) {
         boolean salida = false;
         Iterator<Entry<String,Cliente>> iterator = getClientes().entrySet().iterator();
         while (iterator.hasNext() && !salida) {
@@ -233,7 +236,8 @@ public class CtrlUsuarios implements IDesFavoritear, IConsultaCliente, IConsulta
         return salida;       
     }
     
-    private boolean existeUsuarioNick(String nick) {
+    @Override
+    public boolean existeUsuarioNick(String nick) {
         return getClientes().containsKey(nick) || getArtistas().containsKey(nick);
     }
     
---
+++ servidor_central/src/espotify/interfaces/web/IValidar.java
@@ -0,0 +1,8 @@
+package espotify.interfaces.web;
+
+public interface IValidar {
+    
+    public boolean existeUsuarioNick(String nick);
+    
+    public boolean existeUsuarioCorreo(String correo);
+}
