20a52137db8cfa012401d896e814e1caa9700bf5
Agustina Chaer <aguschaer@gmail.com>
2016-10-14 15:30:55 -0300

    validacion front end

--- servidor_web/src/java/servlets/AltaAlbum_paso2.java
+++ servidor_web/src/java/servlets/AltaAlbum_paso2.java
@@ -7,6 +7,8 @@ package servlets;
 
 import espotify.Fabrica;
 import espotify.datatypes.DataAlbumExt;
+import espotify.datatypes.DataTemaArchivo;
+import espotify.datatypes.DataTemaWeb;
 import espotify.excepciones.ArtistaInexistenteException;
 import espotify.interfaces.web.IAltaAlbumWeb;
 import espotify.interfaces.web.IListarArtistas;
@@ -14,6 +16,7 @@ import espotify.interfaces.web.IListarGeneros;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -32,32 +35,6 @@ import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
  * @author agus
  */
 public class AltaAlbum_paso2 extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     * @throws espotify.excepciones.ArtistaInexistenteException
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, ArtistaInexistenteException {
-        response.setContentType("text/html;charset=UTF-8");
-        HttpSession session = request.getSession();
-        String album = new String(request.getParameter("album").getBytes("iso-8859-1"), "UTF-8");
-
-        IAltaAlbumWeb interf = Fabrica.getIAltaAlbumWeb();
-        DataAlbumExt data = interf.getAlbumTemp((String) session.getAttribute("nick_sesion"), album);
-        if (data != null) {
-            request.setAttribute("album", data);
-            request.getRequestDispatcher("/WEB-INF/albums/paso2.jsp").forward(request, response);
-        }
-
-    }
-
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -69,13 +46,6 @@ public class AltaAlbum_paso2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (ArtistaInexistenteException ex) {
-            response.sendError(500);
-        }
-
-        request.setAttribute("album", new String(request.getParameter("album").getBytes("iso-8859-1"), "UTF-8"));
         request.getRequestDispatcher("/WEB-INF/albums/AltaAlbum/paso2.jsp").forward(request, response);
     }
 
@@ -90,7 +60,43 @@ public class AltaAlbum_paso2 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+       
+        IAltaAlbumWeb inter = Fabrica.getIAltaAlbumWeb();
+        boolean has_errors = false;
+        HttpSession session = request.getSession();
+        try {
+            String artista = (String) session.getAttribute("nick_sesion");
+            DataAlbumExt album = inter.getAlbumTemp( artista, new String(request.getParameter("album").getBytes("iso-8859-1"), "UTF-8"));
 
+            int orden = Integer.parseInt(request.getParameter("orden"));
+            String nombre = new String(request.getParameter("nombre").getBytes("iso-8859-1"), "UTF-8");
+            int duracion = Integer.parseInt(request.getParameter("duracion"));
+            String tipo = request.getParameter("tipo");
+            String url;
+            BufferedInputStream archivo;
+            if (tipo.equals("web")) {
+                url = request.getParameter("url");
+                DataTemaWeb temaWeb = new DataTemaWeb(url, nombre, duracion, orden, artista, album.getNombre());
+                album.addTema(temaWeb);
+            } else if(tipo.equals("archivo")){
+                boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+                if (isMultipart) {
+                    Part part = request.getPart("imagen");
+                    InputStream is = part.getInputStream();
+                    archivo = new BufferedInputStream(is);
+                    DataTemaArchivo temaArchivo = new DataTemaArchivo(archivo, nombre, duracion, orden, artista, album.getNombre());
+                    album.addTema(temaArchivo);
+                }
+            }
+            //Check si quedo agregado
+            if (album.tieneTema(nombre)) {
+                out.print("false");
+            } else {
+                out.print("true");
+            }
+        }catch(ArtistaInexistenteException ex){
+            response.sendError(500);
+        }
     }
 
     /**
--- servidor_web/web/assets/js/script.js
+++ servidor_web/web/assets/js/script.js
@@ -21,20 +21,17 @@ $(".boton-archivo").click( function(){
     $form_archivo.addClass("active");
 });
 
-function crearTema(data) {
-    var datos = JSON.parse(data);
+function crearTema() {
     var $temas = $('.temas');
     var $tema = $temas.children('.tema').first().clone();
-    $tema.children('.orden').text(datos["orden"]);
-    $tema.children('.nombre').text(datos["nombre"]);
-    $tema.children('.duracion').text(datos["duracion"]);
-    $tema.children('.tipo').text(datos["tipo"]);
+    $tema.children('.orden').text($("#orden"));
+    $tema.children('.nombre').text($("#nombre"));
+    $tema.children('.duracion').text($("#duracion"));
+    $tema.children('.tipo').text($("#tipo"));
 
     $temas.append($tema);
     $tema.show();
 }
-
-
 
 function clearFormTema() {
     $('#orden').val("");
@@ -47,10 +44,16 @@ function clearFormTema() {
 
 $("#aceptar").click(function(){
     var has_error = false;
-    var album = $('#album').val();
-    var orden = $('#orden').val();
-    var nombre = $('#nombre').val();
+    var $help = $(".help");
+    var $album = $('#album');
+    var orden;
+    var $orden = $('#orden');
+    var $nombre = $('#nombre');
     var tipo;
+
+    $help.hide();
+    $help.html("");
+
     if($("active").hasClass('form-web')){
         tipo = "web";
     }else{
@@ -60,26 +63,124 @@ $("#aceptar").click(function(){
     var multiplicador = 1;
     var $duracion_str = $('#duracion');
     var duracion_arr = $duracion_str.val().split(":");
+    
+    //VALIDACIONES
+    if($orden.val() === ""){
+        $orden.parent().addClass("has-error");
+        $help.append("El orden del tema es requerido.");
+        has_error = true;
+    }else{
+        orden = $orden.val().parseInt();
+        if(isNaN(orden)){
+            if (!($help.html() === "")) {
+                $help.append("<br />");
+            }
+        $help.append("El orden del tema debe ser numerico.");
+        has_error = true;
+        }else if(orden <= 0){
+            if (!($help.html() === "")) {
+                $help.append("<br />");
+            }
+            $help.append("El orden del tema debe mayor a 0.");
+            has_error = true;
+        }
+    }
+    if ($nombre.val() === ""){
+        $orden.parent().addClass("has-error");
+        if(!($help.html()==="")){
+            $help.append("<br />");
+        }
+        $help.append("El nombre del tema es requerido.");
+        has_error = true;
+    }else{
+        $.ajax({
+            type: "GET",
+            url: "/ValidarNombreTema?album=" + $album.val()+"&tema="+$nombre.val(),
+            success: function (msg) {
+                if (msg === "true") {
+                    $nombre.parent().addClass("has_error");
+                    if (!($help.html() === "")) {
+                        $help.append("<br />");
+                    }
+                    $help.append("El álbum ya tiene un tema con ese nombre.");
+                    has_error = true;
+                }
+            }
+        });
+    }
     if(duracion_arr.length()<1){
         $duracion_str.parent().addClass("has-error");
+        if (!($help.html() === "")) {
+            $help.append("<br />");
+        }
+        $help.append("La duración del tema es requerida");
         has_error = true;
     }else if(duracion_arr.length() > 3){
         $duracion_str.parent().addClass("has-error");
+        if (!($help.html() === "")) {
+            $help.append("<br />");
+        }
+        $help.append("La duración del tema no es correcta");
         has_error = true;
     }else{
+        var parte_duracion;
         for(i= duracion_arr.length()-1; i >= 0; i--){
+            parte_duracion = duracion_arr[i].parseInt();
+            if (isNaN(parte_duracion)) {
+                if (!($help.html() === "")) {
+                    $help.append("<br />");
+                }
+                $help.append("La duracion del tema tiene caracteres inesperados");
+                has_error = true;
+            }else{
             duracion =+ duracion_arr[i]*multiplicador;
             multiplicador *= 60;
+            }
         }
     }
-    var tema = $('.active').find('input').val();
+    var $tema = $('.active').find('input');
+        if($tema.val() === ""){
+            $tema.parent().addClass("has-error");
+            if (!($help.html() === "")) {
+                $help.append("<br />");
+            }
+            $help.append("La URL o archivo del tema es requerido.");
+            has_error = true;
+        }
     if(!has_error){
-        var parameters = '{"orden":"' + orden + '", "nombre":"' + nombre + '", "duracion_f": "' + $duracion_str.val() + '", "duracion": "' + duracion + '", "tipo": "' + tipo + '", "tema": "' + tema + '"}';
-    $.post("/AltaAlbum/paso2?album="+album, parameters, function(data, status){
-        if(status === "success"){
-            crearTema(data);
-            clearFormTema();
-        }
-    }, "json");
+        $.ajax({
+            type: "POST",
+            url: "/AltaAlbum/paso2",
+            data: {
+                orden: orden,
+                nombre: $nombre.val(),
+                duracion: duracion,
+                tipo: tipo,
+                tema: $tema.val()
+            },
+            success: function (msg) {
+                if (msg === "true") {
+                    crearTema();
+                    clearFormTema();
+                }
+            }
+        });
+    }else{
+        $help.show();
     }
-}
+    $(".has-error").each(function(){
+        $(this).removeClass("has-error");
+    });
+});
+
+$(".remove-tema").click(function(){
+    var $nombre = $(this).siblings(".nombre");
+    $.ajax({
+        type: "GET",
+        url: "/BorrarTema?album=" + $(".album").val() + "&tema=" + $nombre.val(),
+        success: function (msg) {
+            $(this).parent().addClass("to-remove");
+            $(document).remove(".to-remove");
+        }
+    });
+});
