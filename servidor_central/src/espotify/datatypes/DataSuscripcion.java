879f45350b97d1b2b6b7e96fe5a7f144052ecdc1
Leandro <leandro.rodriguez@fing.edu.uy>
2016-10-15 15:26:40 -0300

    suscripciones web ok y caso de uso actualizar suscripcion

--- servidor_central/src/espotify/datatypes/DataSuscripcion.java
+++ servidor_central/src/espotify/datatypes/DataSuscripcion.java
@@ -5,17 +5,23 @@ import java.util.Calendar;
 
 public class DataSuscripcion {
     private final Calendar fechaCreacion;
+    private final Calendar fUpdate;
     private final EstadoSuscripcion estado;
     private final TipoSuscripcion tipo;
 
-    public DataSuscripcion(Calendar fechaCreacion, EstadoSuscripcion estado, TipoSuscripcion tipo) {
+    public DataSuscripcion(Calendar fechaCreacion, Calendar fUp,EstadoSuscripcion estado, TipoSuscripcion tipo) {
         this.fechaCreacion = fechaCreacion;
+        this.fUpdate = fUp;
         this.estado = estado;
         this.tipo = tipo;
     }
     
     public Calendar getFechaCreacion() {
         return fechaCreacion;
+    }
+    
+    public Calendar getFechaUpdate() {
+        return fUpdate;
     }
     
     public EstadoSuscripcion getEstado() {
@@ -33,7 +39,12 @@ public class DataSuscripcion {
     
     
     public String info() {
-        return "Estado: " + estado + "tipo: " + tipo + "creada: " + getfCreacionStr();
+        return "Estado: " + estado + " tipo: " + tipo + " Updated: " + getfUpdate();
+    }
+    
+    public String getfUpdate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(fUpdate.getTime());
     }
     
 }
