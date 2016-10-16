025592cd4780a816b0599654d22b9e160066cf1a
JavierM42 <JavierM42@javierm42s-mbp>
2016-10-16 13:07:49 -0200

    checkstyle

--- servidor_web/src/java/servlets/Login.java
+++ servidor_web/src/java/servlets/Login.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package servlets;
 
 import exceptions.UsuarioNoEncontrado;
@@ -22,9 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * @author Igui
- */
 public class Login extends HttpServlet {
    
     /** 
